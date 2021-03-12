438ad802b3069b344112fdb74f96f43f8d388f73
Theo Henson <theodorehenson@protonmail.com>
2021-03-12 17:37:00 -0500

    Create help command and set status

--- commands.py
+++ commands.py
@@ -2,18 +2,26 @@
 
 import server
 
+prefix = '!'
+
 async def ping(msg):
+    """Pings the bot (not the Minecraft server)"""
+
     await msg.reply('pong')
 
 async def up(msg):
+    """Checks if the Minecraft server is up"""
+
     if server.list_ping(False):
         await msg.reply(':white_check_mark: The server is up!')
     else:
         await msg.reply(':no_entry: The server is down.')
 
 async def on(msg):
+    """Prints how many players are on, and a sample of usernames (may not be all online players)"""
+
     if (j := server.list_ping(True)) != None:
-        await msg.reply(f'Players ('+str(j['players']['online'])+'/'+str(j["players"]["max"])+'): '+ \
+        await msg.reply('Players ('+str(j['players']['online'])+'/'+str(j["players"]["max"])+'): '+ \
             ', '.join([p['name'] for p in j['players']['sample']]))
     else:
         await msg.reply(':no_entry: The server is down.')
@@ -24,7 +32,12 @@ unprefixed = {
         'ping': ping
 }
 
-prefix = '!'
-table = {}
+async def help_(msg):
+    content = ''
+    for name in unprefixed:
+        content += '`' + prefix + name + '` — ' + unprefixed[name].__doc__ + '\n'
+    await msg.reply(content)
+
+table = {prefix+'help': help_}
 for k in unprefixed:
     table[prefix+k] = unprefixed[k]
--- msb.py
+++ msb.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+import server
 import discord
 import commands
 
 client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Ready!")
+    await client.change_presence(status=discord.Status.online,
+        activity=discord.Activity(type=discord.ActivityType.watching, name=server.host))
 
 @client.event
 async def on_message(msg):
